37ab1a2c526b5b25d6124cf95d034dc3b6322c00
Pran Pegu <pranpegu997@gmail.com>
2021-08-28 16:09:31 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+export {
+    useCamera,
+    useAutoFocus,
+    useAutoFocusTouch,
+    useBarcodeDetection,
+    useCameraState,
+    useFaceDetection,
+    useFlash,
+    useTextRecognition,
+    useToggleFacing,
+    useWhiteBalance,
+    useZoom,
+} from "./hooks";
+export {
+    CameraOptions,
+    RecordOptions,
+    RecordResponse,
+    TakePictureOptions,
+} from "./interfaces";
